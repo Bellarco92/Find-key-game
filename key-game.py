992900779cf1717ee19630f2da45ca626845d87b
Joanna <joanna.ziemba1@gmail.com>
2023-08-09 22:11:15 +0200

    Add new functions

--- key-game.py
+++ key-game.py
@@ -10,45 +10,64 @@ key_y = randint(0, GAME_HEIGHT)
 player_x = 0
 player_y = 0
 player_found_key = False
+steps = 0
 
+distance_before_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
 
-while not  player_found_key:
+while not player_found_key:
+    steps += 1
     print()
-    print('Moesz udać się w kierunkach określonych jako [W/A/S/D]: ')
+    print('Mozesz udać się w kierunkach określonych jako [W/A/S/D]: ')
 
     move = input('Dokąd idziesz? ')
     match move.lower():
-    case 'w':
-        player_y += 1
-        if player_y > GAME_HEIGHT:
-            print('Ups! Uderzasz w ścianę!')
-            player_y = GAME_HEIGHT
+        case 'w':
+            player_y += 1
 
-    case 's':
-        player_y -= 1
-        if player_y < 0:
-            print('Ups! Uderzasz w ścianę!')
-            player_y = 0
+            if player_y > GAME_HEIGHT:
+                print('Ups! Uderzasz w ścianę!')
+                player_y = GAME_HEIGHT
 
-    case 'a':
-        player_x -= 1
-        if player_x > 0:
-            print('Ups! Uderzasz w ścianę!')
-            player_y = 0
+        case 's':
+            player_y -= 1
 
-    case 'd':
-        player_x += 1
-        if player_x > GAME_WIDTH:
-            print('Ups! Uderzasz w ścianę!')
-            player_x = GAME_WIDTH
-    case 'q':
+            if player_y < 0:
+                print('Ups! Uderzasz w ścianę!')
+                player_y = 0
+
+        case 'a':
+            player_x -= 1
+
+            if player_x < 0:
+                print('Ups! Uderzasz w ścianę!')
+                player_x = 0
+
+        case 'd':
+            player_x += 1
+
+            if player_x > GAME_WIDTH:
+                print('Ups! Uderzasz w ścianę!')
+                player_x = GAME_WIDTH
+
+        case 'q':
+            quit('Koniec gry!')
+        case '-':
+            print('Nie wiem dokąd idziesz...')
+            continue
+
+    if player_x == key_x and player_y == key_y:
+        print('Klucz do skarbu jest Twój!')
+        print(f'Wykonałeś/wykonałaś {steps} ruchów.')
         quit()
-    case '-':
-        print('Nie wiem dokąd idziesz...')
-        continue
 
-if player_x == key_x and player_y == key_y:
-    print('Klucz do skarbu jest Twój!')
+    distance_after_move = sqrt((key_x - player_x) ** 2 + (key_y - player_y) ** 2)
 
-print(player_x, player_y)
+    if distance_before_move > distance_after_move:
+            print('Ciepło!')
+    else: 
+        print('Zimno!')
+
+    distance_before_move = distance_after_move
+
+
 
